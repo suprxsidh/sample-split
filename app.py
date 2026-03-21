0ba8588113ebb68a477e4315f6ad333c28a872b3
Suprasidh <suprasidh.das@gmail.com>
2026-03-21 15:05:54 +0530

    Initialize database on app startup for production

--- app.py
+++ app.py
@@ -629,6 +629,8 @@ def init_db():
     with app.app_context():
         db.create_all()
 
-if __name__ == '__main__':
+with app.app_context():
     init_db()
+
+if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
